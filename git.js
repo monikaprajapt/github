d50635d9c0fb7e824f1a9a2ff10b6aaa9cf67339
Your Name <monikaprajapati20@navgurukul.org>
2022-07-19 13:00:36 +0530

    Initial commit

---
+++ git.js
@@ -0,0 +1,11 @@
+const a=require('readline-sync')
+var i=a.questionInt("enter the number")
+    if (i%2==0){
+        console.log('even number')
+    }
+    else{
+        console.log('odd number')
+    }
+    
+
+
